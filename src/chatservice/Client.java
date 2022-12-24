5876c7568837825a3166ad898ce675496b35db3e
thaichihien <79949235+thaichihien@users.noreply.github.com>
2022-12-24 10:59:44 +0700

    update client

--- src/chatservice/Client.java
+++ src/chatservice/Client.java
@@ -1,16 +1,33 @@
 package chatservice;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.Socket;
+
+import userchatapp.Login;
 
 public class Client {
     Socket client;
     final int PORT = 500;
+    PrintWriter pw;
+    BufferedReader br;
 
     public Client(){
         try {
             client = new Socket(InetAddress.getLocalHost(), PORT);
+            OutputStream clientOut = client.getOutputStream();
+            pw = new PrintWriter(new OutputStreamWriter(clientOut, "UTF-8"), true);
+   
+            // Create an input stream of the client socket
+            InputStream clientIn = client.getInputStream();
+            br = new BufferedReader(new InputStreamReader(clientIn,"UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -19,6 +36,11 @@ public class Client {
     }
 
     public void connect(){
+
+            Login loginForm  = new Login();        
+            
+
+
 
     }
 
