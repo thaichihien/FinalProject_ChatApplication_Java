c66bde8f88fd1e8db7dd1c94fc46f1d815b4f472
nhat2012782 <gonbbb1@gmail.com>
2022-12-26 00:31:16 +0700

    Nhat: done MenuLoginHistory sort

--- src/adminchatapp/MenuLoginHistory.java
+++ src/adminchatapp/MenuLoginHistory.java
@@ -6,7 +6,15 @@ package adminchatapp;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.security.Timestamp;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -37,10 +45,102 @@ public class MenuLoginHistory extends  MenuAdminLayout{
     // sortFilter mặc định gọi hàm database.getAllLoginHistory()
 
 
+    // Phần Nhật làm start:
+    // sửa thời gian đăng nhập chỉ có day ko có time
+    public ArrayList<LoginHistory> getAllLoginHistory(){
+        String SELECT_QUERY = "SELECT LH.*,UA.USERNAME FROM LOGIN_HISTORY LH INNER JOIN USER_ACCOUNT UA ON LH.USER_ID = UA.ID";
+        ResultSet data = null;
+        ArrayList<LoginHistory> loginList = new ArrayList<>();
+        Connection conn = DatabaseManagment.getInstance().getConnection();
+        try (PreparedStatement statment = conn.prepareStatement(SELECT_QUERY,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);){
+            
+            //statment.setString(1, name);
+            data = statment.executeQuery();
+            
+            if(!data.next()){
+                return loginList;
+            }
+            else{
+                
+                
+                do {                    
+                    LoginHistory login = new LoginHistory();
+                    login.setID(data.getInt("LOGIN_ID"));
+                    login.setUserID(data.getInt("USER_ID"));
+                    login.setUserName(data.getString("username"));
+                    java.sql.Timestamp date = data.getTimestamp("LOGIN_TIME");
+                    String formattedDate = new SimpleDateFormat("dd-MM-yyyy HH:mm").format(date);
+                    login.setLoginTime(formattedDate);
+                    loginList.add(login);
+                    
+                } while (data.next());
+                return loginList;
+            }
+            
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            if(data != null){
+                try {
+                    data.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return loginList;
+    }
+
+    public ArrayList<LoginHistory> getAllLoginHistory(String sort,String by){
+        String SELECT_QUERY = "SELECT LH.*,UA.USERNAME FROM LOGIN_HISTORY LH INNER JOIN USER_ACCOUNT UA ON LH.USER_ID = UA.ID ORDER BY " + sort + " " + by ;
+        ResultSet data = null;
+        ArrayList<LoginHistory> loginList = new ArrayList<>();
+        Connection conn = DatabaseManagment.getInstance().getConnection();
+        try (PreparedStatement statment = conn.prepareStatement(SELECT_QUERY,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);){
+            
+            //statment.setString(1, name);
+            data = statment.executeQuery();
+            
+            if(!data.next()){
+                return loginList;
+            }
+            else{
+                
+                
+                do {                    
+                    LoginHistory login = new LoginHistory();
+                    login.setID(data.getInt("LOGIN_ID"));
+                    login.setUserID(data.getInt("USER_ID"));
+                    login.setUserName(data.getString("username"));
+                    java.sql.Timestamp date = data.getTimestamp("LOGIN_TIME");
+                    String formattedDate = new SimpleDateFormat("dd-MM-yyyy HH:mm").format(date);
+                    login.setLoginTime(formattedDate);
+                    loginList.add(login);
+                    
+                } while (data.next());
+                return loginList;
+            }
+            
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            if(data != null){
+                try {
+                    data.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return loginList;
+    }
+
     public void filltableLoginHistory(){
         Utils.clearTable(tableLoginHistory);
-        DatabaseManagment database = DatabaseManagment.getInstance();
-        ArrayList<LoginHistory> allLogin = database.getAllLoginHistory();
+        // DatabaseManagment database = DatabaseManagment.getInstance();
+        ArrayList<LoginHistory> allLogin = getAllLoginHistory();
         // tableFindFriend is JTable
         DefaultTableModel tableModel = (DefaultTableModel) tableLoginHistory.getModel();
         for(LoginHistory login : allLogin){
@@ -52,6 +152,54 @@ public class MenuLoginHistory extends  MenuAdminLayout{
             tableModel.addRow(row);
         }
     }
+
+    public void filltableLoginHistory(String sort,String by){
+        Utils.clearTable(tableLoginHistory);
+        // DatabaseManagment database = DatabaseManagment.getInstance();
+        ArrayList<LoginHistory> allLogin = getAllLoginHistory(sort, by);
+        // tableFindFriend is JTable
+        DefaultTableModel tableModel = (DefaultTableModel) tableLoginHistory.getModel();
+        for(LoginHistory login : allLogin){
+            String loginTime = login.getLoginTime();
+            String userID = String.valueOf(login.getUserID());
+            String username = login.getUserName();
+
+            String row[] = {loginTime,userID,username};
+            tableModel.addRow(row);
+        }
+    }
+
+    public void handleSortCb() {
+
+        ActionListener cbActionListener = new ActionListener() {//add actionlistner to listen for change
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String s = (String) sortFilter.getSelectedItem();//get the selected item
+
+                switch (s) {//check for a match
+                    case "Mặc định":
+                        filltableLoginHistory();
+                        break;
+                    case "Ngày đăng nhập mới":
+                        filltableLoginHistory("LOGIN_TIME", "ASC");
+                        break;
+                    case "Ngày đăng nhập cũ":
+                        filltableLoginHistory("LOGIN_TIME",  "DESC");
+                        break;
+                    default:
+                        System.out.println("No match selected!");
+                        break;
+                }
+            }
+        };
+
+        sortFilter.addActionListener(cbActionListener);
+    }
+
+    // Phần Nhật làm end
+
+
 
     public MenuLoginHistory(JFrame parentFrame) {
         super(parentFrame);
@@ -70,6 +218,7 @@ public class MenuLoginHistory extends  MenuAdminLayout{
 
 
         filltableLoginHistory();
+        handleSortCb();
     }
 
     private void initComponents(){
